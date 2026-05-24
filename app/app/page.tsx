d67f4abc51e5bd7df76c5b76fb613c613812836f
ioankaras <ikaranikolas@epignosishq.com>
2026-05-24 10:49:28 +0300

    Update bill icon

--- app/app/page.tsx
+++ app/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
 
           icon={
             <svg className="w-7 h-7" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M9 14l2 2 4-4m5 4V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12l3-2 3 2 3-2 3 2z" />
+              <path strokeLinecap="round" strokeLinejoin="round" d="M 9 10 l 2 2 l 4 -4 m 5 4 V 6 a 2 2 0 0 0 -2 -2 H 6 a 2 2 0 0 0 -2 2 v 12 l 3 -2 l 3 2 l 3 -2 l 3 2 z" />
             </svg>
           }
         />
